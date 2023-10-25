88a03f21fa8292c81d3f7dfb9fae3de2d8ee2398
Hayden Durham <haydendurham@Haydens-MBP.hsd1.tn.comcast.net>
2023-10-24 23:06:26 -0500

    created a seperate function for downloading the installers

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int startDownload(const char *version);
+
 int main(int argc, char *argv[]) {
 
 
@@ -11,18 +13,26 @@ int main(int argc, char *argv[]) {
   
   char *version = argv[1];
 
-  char command[256];
+  int ret = startDownload(version);
 
-
-
-  snprintf(command, sizeof(command), "softwareupdate --fetch-full-installer --full-installer-version %s", version);
-  printf("%s\n", command);
-
-  int ret = system(command);
   if (ret == 1) {
     perror("system");
     exit(EXIT_FAILURE);
   }
 
   return 0;
+}
+
+int startDownload(const char *version) {	
+  char command[256];
+  snprintf(command, sizeof(command), "softwareupdate --fetch-full-installer --full-installer-version %s", version);
+  printf("%s\n", command);
+
+  int ret = system(command);
+
+  if (ret != 0) {
+	  fprintf(stderr, "Command failed with error code: %d\n", ret);
+  }
+
+  return ret;
 }
